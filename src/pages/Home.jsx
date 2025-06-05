089a7b045415ddcc074bdc11a8f422baa1d07d93
TUMIT_HASAN <tumithasan1@gmail.com>
2025-06-05 12:34:48 +0600

    create banner part of the web site

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Slider from '../components/Slider';
 
 const Home = () => {
     return (
         <div>
-            
+            <section>
+                <Slider></Slider>
+            </section>
         </div>
     );
 };
